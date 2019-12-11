9de862ae6234533314211a9436410a5426155c4f
曹锐 <caorui1@corp.netease.com>
2019-12-11 18:14:17 +0800

    update

--- restful-provider/restful-provider-api-definition/src/main/java/sinosoftsh/provider/api/CalculateApi.java
+++ restful-provider/restful-provider-api-definition/src/main/java/sinosoftsh/provider/api/CalculateApi.java
@@ -1,11 +1,11 @@
 package sinosoftsh.provider.api;
 
-import org.springframework.cloud.netflix.feign.FeignClient;
+
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
-@FeignClient(value = "calculate",path = "/api")
+@FeignClient(value = "calculate",path = "/api2")
 public interface CalculateApi {
 
     @PostMapping(path = "/add")
--- restful-provider/restful-provider-app/src/main/java/sinosoftsh/provider/app/web/Calculate2Controller.java
+++ restful-provider/restful-provider-app/src/main/java/sinosoftsh/provider/app/web/Calculate2Controller.java
@@ -6,21 +6,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import sinosoftsh.provider.api.CalculateApi;
 
-import java.math.BigDecimal;
+import javax.annotation.Resource;
 
 
 @RestController
-@RequestMapping("/api")
-public class CalculateController implements CalculateApi{
+@RequestMapping("/api2")
+public class Calculate2Controller implements CalculateApi{
+    @Resource
+    CalculateApi calculateApi;
 
+    @PostMapping("/add")
     @Override
     public Integer add(@RequestParam Integer a,@RequestParam Integer b){
-        return a+b;
+        return calculateApi.add(a, b);
     }
 
+    @PostMapping("/subtract")
     @Override
     public Integer subtract(@RequestParam Integer a,@RequestParam Integer b){
-        return a-b;
+        return calculateApi.subtract(a, b);
     }
 
 }
